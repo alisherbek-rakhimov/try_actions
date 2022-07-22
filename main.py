abf5ee173af5fecb50a8318c7114f67f606ae652
Alisherbek Rakhimov <alisherrakhimov1997@gmail.com>
2022-07-22 17:55:50 +0500

    second commit

--- main.py
+++ main.py
@@ -3,37 +3,29 @@
 
 # Press <no shortcut> to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from wemake_python_styleguide.visitors.ast.exceptions import WrongTryExceptVisitor
 
 
-def printHi(name: str) -> None:
+def print_hi(name: str) -> None:
     # Use a breakpoint in the code line below to debug your script.
     print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
-    _id = 2
-    a = 2
-    b = 3
-    if a == 1:
-        if b == 4:
-            if a < 5:
-                if b > 12:
-                    if a > -5 and b > -9:
-                        if a == b:
-                            if a * 2 > b:
-                                print("a")
-                                print(id)
-    try:
-        a = b
-    except BaseException:
-        print("catched")
+    alpha = 2
+    beta = 3
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    printHi("PyCharm")
+    print_hi("PyCharm")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 from typing import Dict
 
 
-def a(x: Dict[str, str]) -> None:
-    print(x)
+def a_func(x_name: Dict[str, str]) -> None:
+    """
+
+    :param x_name:
+    :return:
+    """
+    return x_name
