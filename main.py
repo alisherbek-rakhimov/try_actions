63966c7e3489322eb356c332985ffef00ad10b0d
Alisherbek Rakhimov <alisherrakhimov1997@gmail.com>
2022-07-22 18:17:49 +0500

    4th commit

--- main.py
+++ main.py
@@ -16,16 +16,3 @@ def print_hi(name: str) -> None:
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
     print_hi("PyCharm")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-from typing import Dict
-
-
-def a_func(x_name: Dict[str, str]) -> None:
-    """
-
-    :param x_name:
-    :return:
-    """
-    return x_name
